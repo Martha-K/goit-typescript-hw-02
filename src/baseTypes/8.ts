817203a7ea7ad77eeb341271d64848151187d00d
Martha-K <kozenyukmarta@gmail.com>
2023-10-29 23:49:26 +0200

    temporary save

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -2,7 +2,10 @@
   Створіть тип "Gender", використовуючи union type, 
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
+type Gender = {
+  gender: 'male' | 'female';
+}
+const myGender: Gender = {gender: 'male'};
 
-const myGender;
 
-export {};
+export {myGender};
--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,15 +3,14 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
-  return new Promise((resolve) => {
-    resolve(['Text', 50]);
+
+function getPromise<T>() {
+  return new Promise<T>((resolve) => {
+    resolve(["Text", 50] as unknown as T);
   });
 }
 
-getPromise()
-.then((data) => {
+getPromise<[string, number]>().then((data) => {
   console.log(data);
 });
-
-export {};
+export { };
